0226d1b93c173ebe35e25286abe797ef5cb60c4b
v.markitanov <v.markitanov@bftcom.com>
2022-05-03 23:43:48 +0300

    WebSockets simple chat.

---
+++ src/main/kotlin/com/vadmark223/plugins/Connection.kt
@@ -0,0 +1,16 @@
+package com.vadmark223.plugins
+
+import io.ktor.websocket.*
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * @author Markitanov Vadim
+ * @since 03.05.2022
+ */
+class Connection(val session: DefaultWebSocketSession) {
+    companion object{
+        var lastId = AtomicInteger(0)
+    }
+
+    val name = "User ${lastId.getAndIncrement()}"
+}
--- src/main/kotlin/com/vadmark223/plugins/WebSockets.kt
+++ src/main/kotlin/com/vadmark223/plugins/WebSockets.kt
@@ -5,6 +5,7 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import java.time.Duration
+import java.util.*
 
 /**
  * @author Markitanov Vadim
@@ -19,12 +20,26 @@ fun Application.configureSockets() {
     }
 
     routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
         webSocket("/chat") {
-            send("You are connected!")
-            for (frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receivedText = frame.readText()
-                send("You said: $receivedText")
+            send("Add user!!")
+
+            val thisConnection = Connection(this)
+            connections += thisConnection
+
+            try {
+                send("You are connected! There are ${connections.count()}")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach { it.session.send(textWithUsername) }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
             }
         }
     }
